c9953500c0b9cc698dbd127cf443a9a855289143
Brian Lee <ibltsandwich@gmail.com>
2018-11-29 18:17:42 -0500

    Try to add animation

--- src/index.js
+++ src/index.js
@@ -1,14 +1,45 @@
 import { drawZombie, drawText } from "./zombie.js";
 import { drawPlayer } from "./player";
 
+const words = ["hello", "word"];
+
 document.addEventListener('DOMContentLoaded', () => {
   const canvas = document.getElementById("canvas");
   const ctx = canvas.getContext("2d");
   const input = document.getElementById('typing-form');
+  
+  let x = -100;
+  let y = canvas.height / 2 - 70;
+  let dx = 7;
+  let zombieAlive = true;
+  let health = 100;
+  let shift = 0;
+  const frameWidth = 100;
+  let totalFrames = 12;
+  let currentFrame = 0;
 
-  const words = ["hello"];
-  drawZombie(ctx, "hello");
-  drawPlayer(ctx);
+  function draw () {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    if (zombieAlive === true) {
+      drawZombie(ctx, "hello", x, y, shift)
+    }
+    if (x < canvas.width - 200) {
+      x+=dx;
+      shift+=frameWidth + 1
+      currentFrame += 1
+      if (currentFrame === totalFrames) {
+        shift = 0;
+        currentFrame = 0;
+      }
+    } else if (health > 0) {
+      health -= .5
+      console.log(health)
+    }
+
+    if (health > 0) {
+      drawPlayer(ctx);
+    }
+  }
 
   input.addEventListener('keyup', handleZombie)
   function handleZombie (e) {
@@ -16,6 +47,7 @@ document.addEventListener('DOMContentLoaded', () => {
       ctx.clearRect(0, 0, canvas.width, canvas.height);
       console.log(input.value);
       input.value = "";
+      zombieAlive = false;
       drawPlayer(ctx);
     } else if (e.keycode === 13) {
       console.log(input.value);
@@ -25,5 +57,5 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
   
-  
+  setInterval(draw, 80);
 })
--- src/zombie.js
+++ src/zombie.js
@@ -1,10 +1,26 @@
-export const drawZombie = (ctx, word) => {
+
+const frameWidth = 100;
+const frameHeight = 144;
+
+function animate(ctx, image, x, y, shift) {
+  ctx.drawImage(image, shift, 165, frameWidth, frameHeight,
+  x, 150, frameWidth, frameHeight)
+}
+
+
+export const drawZombie = (ctx, word, x, y, shift) => {
+
+  const zombieImage = new Image();
+  zombieImage.src = "../assets/zombie_sprite.png";
+
+  animate(ctx, zombieImage, x, y, shift);
+
   ctx.beginPath();
   ctx.fillStyle = "#F8030F";
-  ctx.fillRect(150, canvas.height / 2 - 70, 35, 70);
+  ctx.fillRect(x, y, 35, 70);
   ctx.fillStyle = "black";
   ctx.font = "20pt sans-serif";
-  ctx.fillText(word, 150, canvas.height / 2 - 70);
+  ctx.fillText(word, x-8, y-5);
   ctx.closePath();
 }
 
@@ -12,8 +28,8 @@ export const drawText = (ctx) => {
   ctx.beginPath();
 
   ctx.fillStyle = "red";
-  ctx.fillRect(150, canvas.height/2 - 70, 35, 70);
+  ctx.fillRect(x, y, 35, 70);
   ctx.fillStyle = "black";
   ctx.font = "20pt sans-serif";
-  ctx.fillText("word1", 150, canvas.height / 2 - 70);
+  ctx.fillText("word1", 145, canvas.height / 2 - 70);
 }
